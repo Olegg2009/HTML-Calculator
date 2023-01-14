a5b4ca473053a56fc2e2bc6cd052109b8243bec4
Olegg2009 <95966544+Olegg2009@users.noreply.github.com>
2023-01-14 17:34:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function sum() {
+    let x = document.getElementById('num1').value;
+    let y = document.getElementById('num2').value;
+    document.getElementById('result').innerHTML = (+x + +y)
+}
+function min() {
+    let x = document.getElementById('num1').value;
+    let y = document.getElementById('num2').value;
+    document.getElementById('result').innerHTML = (+x - +y)
+}
+function mul() {
+    let x = document.getElementById('num1').value;
+    let y = document.getElementById('num2').value;
+    document.getElementById('result').innerHTML = (+x * +y)
+}
+function del() {
+    let x = document.getElementById('num1').value;
+    let y = document.getElementById('num2').value;
+    document.getElementById('result').innerHTML = (+x / +y)
+}
